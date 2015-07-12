7b94ff66e249c5d802e83d839c7b68f3bbc88b8a
Andrew Snow <andrew.j.snow@gmail.com>
2015-07-12 17:52:41 -0500

    Initial Commit

---
+++ Shear/Shear.h
@@ -0,0 +1,19 @@
+//
+//  Shear.h
+//  Shear
+//
+//  Created by Andrew Snow on 7/12/15.
+//  Copyright © 2015 Andrew Snow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Shear.
+FOUNDATION_EXPORT double ShearVersionNumber;
+
+//! Project version string for Shear.
+FOUNDATION_EXPORT const unsigned char ShearVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shear/PublicHeader.h>
+
+
