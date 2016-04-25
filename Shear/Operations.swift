0f1a81ca6554c40167de52fb01cf2fd030386adb
Andrew Snow <andrew.j.snow@gmail.com>
2016-04-25 16:56:42 -0700

    Add: .laminate() and .interpose() operations
    This allows clients to stitch together Arrays more easily than DenseArray's collection of Arrays constructors.

--- Shear/Operations.swift
+++ Shear/Operations.swift
@@ -110,34 +110,68 @@ public extension Array {
     
     /// Returns a DenseArray with the contents of additionalItems appended to the last axis of the Array.
     /// Note: this addes extra length in all but the last dimension, it does not change the shape.
-    /// Use DenseArray(collection: [Arrays]) to make a higher order Array.
     public func append<A: Array where A.Element == Element>(additionalItems: A) -> DenseArray<Element> {
         return zipVectorMap(self, additionalItems, byRows: true, transform: {$0 + $1})
     }
     
     /// Returns a DenseArray with the contents of additionalItems concatenated to the first axis of the Array.
     /// Note: this addes extra length in all but the first dimension, it does not change the shape.
-    /// Use DenseArray(collection: [Arrays]) to make a higher order Array.
     public func concat<A: Array where A.Element == Element>(additionalItems: A) -> DenseArray<Element> {
         return zipVectorMap(self, additionalItems, byRows: false, transform: {$0 + $1})
     }
     
     /// Returns a DenseArray with the contents of additionalItems appended to the last axis of the Array.
-    /// Note: this addes extra length in all but the last dimension, it does not change the shape.
-    /// Use DenseArray(collection: [Arrays]) to make a higher order Array.
+    /// e.g.
+    ///    1 2 | 5 6 --> 1 2 5 6
+    ///    3 4 | 7 8 --> 3 4 7 8
     public func append(additionalItem: Element) -> DenseArray<Element> {
         return vectorMap(byRows: true, transform: {$0 + [additionalItem]})
     }
     
     /// Returns a DenseArray with the contents of additionalItems concatenated to the first axis of the Array.
-    /// Note: this addes extra length in all but the first dimension, it does not change the shape.
-    /// Use DenseArray(collection: [Arrays]) to make a higher order Array.
+    /// e.g.
+    ///    1 2 | 5 6 --> 1 2
+    ///    3 4 | 7 8 --> 3 4
+    ///                  5 6
+    ///                  7 8
     public func concat(additionalItem: Element) -> DenseArray<Element> {
         return vectorMap(byRows: false, transform: {$0 + [additionalItem]})
         // This could be optimized to the following:
         //     return DenseArray(shape: [shape[0] + 1] + shape.dropFirst(), baseArray: [Element](allElements) + [Element](count: shape.dropFirst().reduce(*), repeatedValue: additionalItem))
         // But given all preformance we're leaving on the table elsewhere, it seems silly to break the nice symmetry for an unimportant function.
         // I've also not benchmarked the difference, so this could turn out to be a pessimization, though that would shock me.
+    }
+    
+    /// Returns a DenseArray of a higher order by creating a new first axis. Both input arrays must be the same shape.
+    /// e.g.
+    ///    1 2 3 4 | 5 6 7 8 --> 1 2 3 4
+    ///                          5 6 7 8
+    public func laminate<A: Array where A.Element == Element>(additionalItems: A) -> DenseArray<Element> {
+        guard shape == additionalItems.shape else {
+            fatalError("Arrays must have same shape to be laminated")
+        }
+        
+        let newShape = [2] as [Int] + shape
+        let items = allElements.map { $0 } + additionalItems.allElements.map { $0 }
+    
+        return DenseArray(shape: newShape, baseArray: items)
+    }
+    
+    /// Returns a DenseArray of a higher order by creating a new last axis. Both input arrays must be the same shape.
+    /// e.g.
+    ///    1 2 3 4 | 5 6 7 8 --> 1 5
+    ///                          2 6
+    ///                          3 7
+    ///                          4 8
+    public func interpose<A: Array where A.Element == Element>(additionalItems: A) -> DenseArray<Element> {
+        guard shape == additionalItems.shape else {
+            fatalError("Arrays must have same shape to be interposed")
+        }
+        
+        let newShape = shape + [2] as [Int]
+        let items = zip(allElements, additionalItems.allElements).reduce([] as [Element]) { $0 + [$1.0, $1.0] }
+        
+        return DenseArray(shape: newShape, baseArray: items)
     }
     
 }
