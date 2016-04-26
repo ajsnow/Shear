562def1d995db3a957e0811ff9cab945b98893ae
Andrew Snow <andrew.j.snow@gmail.com>
2016-04-25 17:11:37 -0700

    Add: Tests for .laminate() and .interpose()

--- ShearTests/OpertaionsTests.swift
+++ ShearTests/OpertaionsTests.swift
@@ -158,8 +158,8 @@ class OpertaionsTests: XCTestCase {
         XCTAssert(iotaCube.concat(iotaRect) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 0, 1, 2, 3, 4, 5, 6, 7, 8].reshape([4, 3, 3]))
         
         let conIotaRect = iotaRect.laminate(iotaRect).concat(iotaRect)
-        print(iotaCube.laminate(conIotaRect))
-        print(iotaCube.interpose(conIotaRect))
+        XCTAssert(iotaCube.laminate(conIotaRect) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 1, 2, 3, 4, 5, 6, 7, 8].reshape([2, 3, 3, 3]))
+        XCTAssert(iotaCube.interpose(conIotaRect) == [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26].reshape([3, 3, 3, 2]))
     }
 
 //    func testMaps() {
